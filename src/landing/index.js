dde310934c1a205eb28bd91ab7767abb14d7dc55
Matthew Day <matt@matthewday.net>
2018-01-04 17:44:31 -0500

    Remove extraneous code

--- src/landing/index.js
+++ src/landing/index.js
@@ -9,35 +9,22 @@ import {debounce} from '../helpers';
 class Landing extends Component {
   constructor(props) {
     super(props);
-    this.state = { width: '0', height: '0' };
     this.handleClick = this.handleClick.bind(this);
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
   handleClick() {
     this.props.dispatch(loginUser(this.props.loggedIn));
   }
 
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-    console.log(window.innerWidth, window.innerHeight);
-  }
-
   componentDidMount() {
-    this.updateWindowDimensions();
     Events.scrollEvent.register('begin', function() {
       console.log("begin", arguments);
     });
     Events.scrollEvent.register('end', function() {
       console.log("end", arguments);
     });
-    window.addEventListener('resize', debounce(this.updateWindowDimensions, 400));
     window.addEventListener('scroll', this.toggleNavigation);
   }
-
-
-  // window.addEventListener('resize', JD.debounce(JD.lastName, 400));
-  // window.addEventListener('resize', JD.debounce(JD.firstName, 400, true));
 
   scrollTo() {
     scroller.scrollTo('scroll-to-element', {
@@ -75,7 +62,6 @@ class Landing extends Component {
   componentWillUnmount() {
     Events.scrollEvent.remove('begin');
     Events.scrollEvent.remove('end');
-    window.removeEventListener('resize', this.updateWindowDimensions);
   }
 
   render() {
@@ -90,7 +76,7 @@ class Landing extends Component {
               <Style.item><a onClick={this.handleClick}>Login</a></Style.item>
             </Style.navs>
           </Style.box>
-          <Link to="clips" spy={true} smooth={true} duration={500}><Style.arrow src={arrow} className={this.state.fixedNavs} /></Link>
+          <Link to="clips" spy={true} smooth={true} duration={500}><Style.arrow src={arrow} /></Link>
         </Style.imageW>
 
       </div>
